73efbe708a0a793bf71b4f37ece19e6a7cf56070
Kirill Lappo <kirill-lappo@outlook.com>
2024-02-26 23:06:32 +0300

    compose update

--- compose/gates.entrypoint.sh
+++ compose/gates.entrypoint.sh
@@ -1,16 +1,15 @@
 set -e
 
-sleepTime=30s
+sleepTime=1m
 echo "Waiting for $sleepTime to let the db warm up"
 echo "Database starts to listen to port immediately; however, it may take time to init SQL Server"
+sleep $sleepTime
 
 portWait=30
 echo "Waiting for MSSQL port is accessible for $portWait seconds"
 /tools/wait-for-it.sh database:1433 -t $portWait
 
-sleep $sleepTime
-
 echo "Setting up initial user and empty kh database"
-/opt/mssql-tools/bin/sqlcmd -S database -U sa -P $MSSQL_SA_PASSWORD -d master -i /tools/init.sql -e
+/opt/mssql-tools/bin/sqlcmd -S database -U sa -P $MSSQL_SA_PASSWORD -d master -i /tools/database-init.sql -e
 
 echo "Database Gates are opened"
